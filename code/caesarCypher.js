e60474811ce1724b0b51300dc887ceb2487c15b1
Gepoverlow <gepeoverload@gmail.com>
2022-01-26 13:59:48 +0100

    added ceaserCyphon function and test. working good

--- code/caesarCypher.js
+++ code/caesarCypher.js
@@ -1,5 +1,44 @@
 function caesarCypher(string) {
-  return "efgfoe uif fbtu xbmm pg uif dbtumf";
+  return numbersToString(stringToNumbers(string));
+}
+
+let alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+function isLetter(letter) {
+  return letter.toUpperCase() != letter.toLowerCase() ? true : false;
+}
+
+function isNumber(number) {
+  return typeof number === "number" ? true : false;
+}
+
+function stringToNumbers(string) {
+  let result = [];
+
+  string = string
+    .replace(/\W*\d+/g, "")
+    .toLowerCase()
+    .split("");
+
+  for (let i = 0; i < string.length; i++) {
+    isLetter(string[i])
+      ? result.push(alphabet.indexOf(string[i]) + 1)
+      : result.push(string[i]);
+  }
+
+  return result;
+}
+
+function numbersToString(number) {
+  let result = [];
+
+  for (let i = 0; i < number.length; i++) {
+    isNumber(number[i])
+      ? result.push(alphabet[number[i]])
+      : result.push(number[i]);
+  }
+
+  return result.join("");
 }
 
 module.exports = caesarCypher;
--- tests/caesarCypher.test.js
+++ tests/caesarCypher.test.js
@@ -5,3 +5,9 @@ test("CaesarCypher Error 1", () => {
     "efgfoe uif fbtu xbmm pg uif dbtumf"
   );
 });
+
+test("CaesarCypher Error 2", () => {
+  expect(caesarCypher("defend. the- east? wall of the castle")).toBe(
+    "efgfoe. uif- fbtu? xbmm pg uif dbtumf"
+  );
+});
